91eb2ee3f7c7f5cedc673017becbc34c5b28bacf
Fmeech2 <83023261+Fmeech2@users.noreply.github.com>
2022-07-13 00:40:15 +0400

    4-я мобильная страница готова
    v1.0.0.5
    Мобильная версия редактирования профиля готова (если не учитывать не доделанный футер на каждой странице для мобилок)

--- index2.php
+++ index2.php
@@ -43,7 +43,7 @@
                                 <li class="mobbail_shapka"><a href="/" class="nav-link px-2 text-secondary"> Оставить заявку</a></li>                                
                                 <li class="mobbail_shapka" id="sh_mobbail"><a href="/MySQL/NewOldNewMessageMobile.php" class="nav-link px-2 text-white"> Сообщения </a></li> <!--//для телефона-->
                                 <li class="mobbail_shapka" id="sh_pk"><a href="/MySQL/NewOldNewMessage.php" class="nav-link px-2 text-white"> Сообщения  </a></li>  <!--//для пк-->
-                       
+                                <li class="mobbail_shapka" id="sh_pk"><a href="/profil" class="nav-link px-2 text-white"> Редактировать профиль </a></li>
                             </ul>
 
                             <div class="text-end">
